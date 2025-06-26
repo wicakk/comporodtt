261efb8674d4cf410ecd1fc1f3d13346b59a2e6d
admin <admin@MacBook-Pro-admin.local>
2025-06-26 09:50:23 +0700

    perbaikan

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make(env('USER_PASSWORD', 'defaultPassword')), // password
+            'password' => bcrypt(env('DEFAULT_PASSWORD', 'fallbackPassword')), // password
             'remember_token' => Str::random(10),
         ];
     }
